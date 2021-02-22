3ef5389cc1f7cc7adacf3fb523c652b14f42a0e2
OhMyGuus <info@guus.ninja>
2021-02-23 00:58:54 +0100

    Temp fix for full history

--- RepBotTest2/lib/TimeSpanHelper.cs
+++ RepBotTest2/lib/TimeSpanHelper.cs
@@ -17,5 +17,10 @@ namespace RepBot.Modules
                 timeString = timeElapsed.Seconds.ToString() + " seconds";
             return timeString;
         }
+
+        public static string WithMaxLength(this string value, int maxLength)
+        {
+            return value?.Substring(0, Math.Min(value.Length, maxLength));
+        }
     }
 }
